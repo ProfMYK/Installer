3316da5fc83aee6a7a63b70bff26586e3a31edc7
Fortran <ProfMYK@proton.me>
2023-02-02 20:58:23 +0300

    added readme

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set +x
-
-wget https://raw.githubusercontent.com/ProfMYK/Installer/main/installer.py
-sudo apt update
-sudo apt install python3 python3-pip
-sudo pip3 install colorama typer inquirer
-python3 installer.py
